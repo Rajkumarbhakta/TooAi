d7f6dbcc4baefe066ea24f34005f284dee093434
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2024-03-12 09:32:26 +0000

    icon added

--- app/src/main/java/com/rkbapps/tools/MainActivity.kt
+++ app/src/main/java/com/rkbapps/tools/MainActivity.kt
@@ -22,9 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Navigator(HomeScreen()) {
-
-                    }
+                    Navigator(HomeScreen())
                 }
             }
         }
