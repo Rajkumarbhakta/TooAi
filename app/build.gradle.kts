bcce99173073427e9f2f06c5958b75c67d7009a2
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2025-07-26 15:19:58 +0530

    update with latest version compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.rkbapps.tooai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
